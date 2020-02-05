ccea6032bcb386876d63a70e7b632e1eb80d043b
lonelywolf.lee <lonelywolf.lee@gmail.com>
2020-02-06 07:47:47 +0900

    Add w03 lecture cont.

---
+++ w03/02_struct.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+struct Car {
+  char gamer_id[20];
+  int fuel_gauge;
+  int current_speed;
+};
+
+int main() {
+  Car car1 = {"no 1", 100, 0};
+
+}
---
+++ w03/06_function_insert.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+struct Car {
+  char gamer_id[20];
+  int fuel_gauge;
+  int current_speed;
+
+  void show_car_state() {
+    cout << "게이머 ID: " << gamer_id << endl;
+    cout << "현재 연료: " << fuel_gauge << endl;
+    cout << "현재 속도: " << current_speed << endl;
+  }
+
+  void accel() {
+    if(fuel_gauge <= 0) return;
+    else fuel_gauge -= 2;
+
+    if(current_speed + 10 >= 200) {
+      current_speed = 200;
+      return;
+    }
+    current_speed += 10;
+  }
+  void push_break() {
+    if(current_speed < 10) {
+      current_speed = 0;
+      return;
+    }
+    current_speed -= 10;
+  }
+};
+
+
+
+int main() {
+  // struct Car car1 = {"no 1", 100, 0};
+  Car car1 = {"no 1", 100, 0};
+  car1.accel();
+	car1.accel();
+	car1.show_car_state();
+	car1.push_break();
+	car1.show_car_state();
+
+  return 0;
+}
+
