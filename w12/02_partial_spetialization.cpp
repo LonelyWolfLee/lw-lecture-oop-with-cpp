733035836cb10bfa7d5b2fa494f6d3e141fcbdfa
lonelywolf <lonelywolf.lee@gmail.com>
2020-05-15 11:08:03 +0900

    update w12-2

--- w12/02_partial_spetialization.cpp
+++ w12/02_partial_spetialization.cpp
@@ -6,7 +6,20 @@ class Sample {};
 template <typename T1>
 class Sample<T1, int> {};
 
+template <typename T1>
+class Sample<char, T1>
+{
+    Sample() {
+
+    }
+};
+
 // 특수화 => 자료형 (타입)
 template <>
 class Sample<char, int> {};
 
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
